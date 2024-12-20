2a84d4e1a98c3af4ceab26114725f4be6f5a351a
AlexCode-dot <alle7000.andersson@gmail.com>
2024-12-20 14:28:34 +0100

    feat(main.js & FrontPage-content.json) added content in json files and functionality to fetch the data

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,13 @@
-if (true) {
-  console.log('hej')
+//Fetch FrontPage Content
+
+loadFrontPageContent()
+
+async function loadFrontPageContent() {
+  const contentResponse = await fetch('FrontPage-content.json')
+  const frontPageContent = await contentResponse.json()
+  console.log(frontPageContent)
+
+  const imagesResponse = await fetch('FrontPage-images.json')
+  const frontPageImages = await imagesResponse.json()
+  console.log(frontPageImages)
 }
