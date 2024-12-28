a07b242eb532e08890e55bb32f1317b0b5d01780
Patseroni <patrik.bustad@lerniastudent.se>
2024-12-28 19:25:59 +0100

    tech(fetch.js) add function to fetch movie-API from json-file

---
+++ Script/fetch.js
@@ -0,0 +1,17 @@
+fetchAPI();
+
+async function fetchAPI() {
+    const url = 'fake-MovieAPI.json';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
