8767790afa016b9b748bc87e4237d64b4c99a441
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2024-12-29 19:22:20 +0100

    (feat)adjusted html (style)moviecards modal

--- Script/moviecards-modal.js
+++ Script/moviecards-modal.js
@@ -1,1 +1,3 @@
 //This is code for card 3.3.3 (Function to display movie information about a specific clicked movie)
+
+const movieContainer = document.querySelector('.movicards-modal__rootcontainer')
