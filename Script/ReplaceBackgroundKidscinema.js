1befdd9b31babc6e333082ad2482e7c419684036
Hungryhippoafk <helsing1993@hotmail.com>
2025-01-01 22:49:29 +0100

    Created an transistion with zoom effect while clicking barnbio or filmer

--- Script/ReplaceBackgroundKidscinema.js
+++ Script/ReplaceBackgroundKidscinema.js
@@ -1,12 +1,25 @@
 document.getElementById('header__children-movies').addEventListener('click', function () {
   const body = document.body
-  // ballooons background
-  body.style.backgroundImage = 'url("assets/images/backgrounds/balloons.png")'
+
+  setTimeout(function () {
+    // Add barn-bio class to zoom in
+    body.classList.add('barn-bio')
+    body.classList.remove('filmer') // Ensure filmer class is removed
+
+    // Set background to balloons
+    body.style.backgroundImage = 'url("assets/images/backgrounds/balloons.png")'
+  }, 200) // Wait for opacity transition to finish
 })
 
 document.getElementById('header__menu').addEventListener('click', function () {
   const body = document.body
 
-  // Set background back to main
-  body.style.backgroundImage = 'url("assets/images/backgrounds/background_main.webp")'
+  setTimeout(function () {
+    // Add filmer class to zoom out
+    body.classList.add('filmer')
+    body.classList.remove('barn-bio') // Ensure barn-bio class is removed
+
+    // Set background back to main
+    body.style.backgroundImage = 'url("assets/images/backgrounds/background_main.webp")'
+  }, 200)
 })
