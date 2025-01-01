eedee502ab627537919e13eaec96f66a76a374b8
Hungryhippoafk <helsing1993@hotmail.com>
2025-01-01 20:55:10 +0100

    Added an listeners to change back if user clicks filmer, removed change to barnbio.

--- Script/ReplaceBackgroundKidscinema.js
+++ Script/ReplaceBackgroundKidscinema.js
@@ -1,10 +1,12 @@
 document.getElementById('header__children-movies').addEventListener('click', function () {
   const body = document.body
+  // ballooons background
+  body.style.backgroundImage = 'url("assets/images/backgrounds/balloons.png")'
+})
 
-  // Check the current background and replace it
-  if (body.style.backgroundImage.includes('balloons.png')) {
-    body.style.backgroundImage = 'url("assets/images/backgrounds/background_main.webp")'
-  } else {
-    body.style.backgroundImage = 'url("assets/images/backgrounds/balloons.png")'
-  }
+document.getElementById('header__menu').addEventListener('click', function () {
+  const body = document.body
+
+  // Set background back to main
+  body.style.backgroundImage = 'url("assets/images/backgrounds/background_main.webp")'
 })
